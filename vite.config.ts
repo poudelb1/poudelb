4b575dd6cba47343cdccd8143f59c626bae78685
Biswas Poudel <108428083+poudelb1@users.noreply.github.com>
2025-02-15 19:32:27 -0600

    Updated vite.config.ts to include the base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Since you're using a custom domain, keep it as '/'
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
